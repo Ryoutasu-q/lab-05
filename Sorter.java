b9759293877fd8f245abc85511674b91e69ed654
JAXBRS <97792185+JAXBRS@users.noreply.github.com>
2022-10-07 16:17:13 +0300

    Update Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -1,18 +1,45 @@
+import java.util.Arrays;
+
 public class Sorter {
-    public static void sortAscending(final int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minElementIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[minElementIndex] > arr[j]) {
-                    minElementIndex = j;
-                }
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /*
+             * Move elements of arr[0..i-1], that are
+             * greater than key, to one position ahead
+             * of their current position
+             */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-    
-            if (minElementIndex != i) {
-                int temp = arr[i];
-                arr[i] = arr[minElementIndex];
-                arr[minElementIndex] = temp;
-            }
+            arr[j + 1] = key;
         }
     }
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[]) {
+        int arr[] = { 12, 11, 13, 5, 6 };
+        Sort(arr);
+    }
+
+    public static void Sort(int array[]) {
+        Sorter ob = new Sorter();
+        ob.sort(array);
+
+        printArray(array);
+    }
+
 }
